e13d6cc836720ce5e1f5a2d47ab0191511b29ab2
Andrew Burgess <developerandrewburgess@outlook.com>
2021-07-06 13:35:00 +0000

    changed hello message

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello everyone from Ben Allwood'
+    return 'Hello everyone from Andrew'
 
 if __name__ == '__main__':
     app.run()
